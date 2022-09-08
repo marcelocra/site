5b4d82c31328e7104c77d977ac0e64df2bdc6656
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2022-09-07 23:31:17 -0300

    Update blog settings and organize file

--- main.tsx
+++ main.tsx
@@ -1,33 +1,39 @@
 /** @jsx h */
 
-import blog, { ga, redirects, h } from "blog";
+import blog, { h } from "blog";
 
 blog({
-	title: "Marcelo Almeida",
-	description: "Um blog de programação",
-	// header: <header>Your custom header</header>,
-	// section: <section>Your custom section</section>,
-	// footer: <footer>Your custom footer</footer>,
-	avatar: "https://github.com/marcelocra.png?size=100",
-	avatarClass: "rounded-full",
-	author: "Marcelo Almeida",
-	lang: "pt-BR",
-	favicon: "./logo.svg",
-	links: [
-		{ title: "Email", url: "mailto:dev@marcelocra.com" },
-		{ title: "GitHub", url: "https://github.com/marcelocra" },
-		{ title: "Telegram", url: "https://t.me/marcelocra" },
-	],
-	// middlewares: [
+  title: "Marcelo Almeida",
+  author: "Marcelo Almeida",
+  description: "Um blog de programação",
+  avatar: "https://github.com/marcelocra.png?size=100",
+  avatarClass: "rounded-full",
+  favicon: "./logo.svg",
 
-	// If you want to set up Google Analytics, paste your GA key here.
-	// ga("UA-XXXXXXXX-X"),
+  lang: "pt-br",
+  theme: "dark",
+  dateStyle: "medium",
+  port: 1111,
 
-	// If you want to provide some redirections, you can specify them here,
-	// pathname specified in a key will redirect to pathname in the value.
-	// redirects({
-	//  "/hello_world.html": "/hello_world",
-	// }),
+  links: [
+    { title: "Email", url: "mailto:dev@marcelocra.com" },
+    { title: "GitHub", url: "https://github.com/marcelocra" },
+    {
+      title: "Telegram",
+      url: "https://t.me/marcelocra",
+      icon: <img style="padding:7px" src="./msg.svg"></img>,
+    },
+    // { title: "Instagram", url: "https://instagram.com/marcelocra" },
+  ],
 
-	// ]
+  middlewares: [
+    // If you want to set up Google Analytics, paste your GA key here.
+    // ga("UA-XXXXXXXX-X"),
+
+    // If you want to provide some redirections, you can specify them here,
+    // pathname specified in a key will redirect to pathname in the value.
+    // redirects({
+    //   // "/hello-world.html": "/hello-world",
+    // }),
+  ],
 });
