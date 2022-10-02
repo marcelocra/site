799a77958e333a0ed50b639fda6a9593ddc74c03
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2022-10-02 09:30:20 -0300

    Add deno adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
+import deno from "@astrojs/deno";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.marcelocra.dev",
   integrations: [mdx(), sitemap(), tailwind(), solidJs()],
+  output: "server",
+  adapter: deno(),
 });
