4b83be2b5c1177a530a95988c30fb5f0d4eacaa8
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2023-01-23 04:56:47 +0000

    fix: fix incorrectly removed site description

--- src/config.ts
+++ src/config.ts
@@ -2,3 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Site do Marcelo";
+export const SITE_DESCRIPTION = "Um blog de programação!";
