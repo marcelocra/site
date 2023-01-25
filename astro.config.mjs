4cbe5ef3224f2b3bc4c787650db4cfa1fa2cd072
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2023-01-25 13:33:57 +0000

    feat!: upgrade to astro 2; add svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import fs from "node:fs";
-
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 import deno from "@astrojs/deno";
+import svelte from "@astrojs/svelte";
 
-const BASE_URL = "https://marcelocra.dev";
-
+const BASE_URL = "https://marcelocra.dev/";
 const mdPagesPath = fs
   .readdirSync("./src/pages/blog")
   .map((page) => `${BASE_URL}/blog/${page.slice(0, -3)}`);
@@ -23,6 +22,7 @@ export default defineConfig({
     }),
     tailwind(),
     solidJs(),
+    svelte(),
   ],
   output: "server",
   adapter: deno(),
