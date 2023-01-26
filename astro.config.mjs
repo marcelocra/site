0bbbfe16bf7a4187a86fb4a230da2685c0ec0f23
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2023-01-26 01:56:01 +0000

    fix: get partytown to work with gtm

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,16 +7,30 @@ import solidJs from "@astrojs/solid-js";
 import deno from "@astrojs/deno";
 import svelte from "@astrojs/svelte";
 import partytown from "@astrojs/partytown";
-const BASE_URL = "https://marcelocra.dev/";
-const mdPagesPath = fs.readdirSync("./src/pages/blog").map(page => `${BASE_URL}/blog/${page.slice(0, -3)}`);
 
+const BASE_URL = "https://marcelocra.dev/";
+const mdPagesPath = fs
+  .readdirSync("./src/pages/blog")
+  .map((page) => `${BASE_URL}/blog/${page.slice(0, -3)}`);
 
 // https://astro.build/config
 export default defineConfig({
   site: BASE_URL,
-  integrations: [mdx(), sitemap({
-    customPages: [`${BASE_URL}/`, `${BASE_URL}/about`, ...mdPagesPath]
-  }), tailwind(), solidJs(), svelte(), partytown()],
+  integrations: [
+    mdx(),
+    sitemap({
+      customPages: [`${BASE_URL}/`, `${BASE_URL}/about`, ...mdPagesPath],
+    }),
+    tailwind(),
+    solidJs(),
+    svelte(),
+    partytown({
+      config: {
+        // Google Tag Manager
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   output: "server",
-  adapter: deno()
+  adapter: deno(),
 });
