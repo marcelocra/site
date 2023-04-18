4bb08c1baa41dac4b71a91ece64da44392576d55
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2023-04-18 10:15:44 -0300

    Add LinkedIn link

--- src/pages/link/[link].js
+++ src/pages/link/[link].js
@@ -1,6 +1,7 @@
 const linkTable = new Map(
   Object.entries({
     cv: "https://www.canva.com/design/DAFfJBLsVSs/bNFNumjdhWy4ZldHkvSKOQ/view?utm_content=DAFfJBLsVSs&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink",
+    linkedin: "https://www.linkedin.com/in/marcelocra",
   })
 );
 
