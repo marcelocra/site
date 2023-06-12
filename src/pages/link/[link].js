6235c8a688e0a24c6e05184dbe7b01934f2a88a9
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2023-06-12 10:05:32 -0300

    Add link to vip testers form

--- src/pages/link/[link].js
+++ src/pages/link/[link].js
@@ -3,6 +3,7 @@ const linkTable = new Map(
     cv: "https://www.canva.com/design/DAFfJBLsVSs/bNFNumjdhWy4ZldHkvSKOQ/view?utm_content=DAFfJBLsVSs&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink",
     linkedin: "https://www.linkedin.com/in/marcelocra",
     whatsapp: "https://wa.me/5531971320866",
+    vip: "https://docs.google.com/forms/d/e/1FAIpQLSeEvsAlYs96vbtXMNGPxGRTStibO9rn5TmLF9374HaKCt1c3g/viewform?usp=sf_link",
   })
 );
 
