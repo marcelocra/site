ad457a55955ac4ae7bb7af26c5496f252325a84e
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2023-06-21 09:06:41 -0300

    Update [link].js

--- src/pages/link/[link].js
+++ src/pages/link/[link].js
@@ -4,6 +4,7 @@ const linkTable = new Map(
     linkedin: "https://www.linkedin.com/in/marcelocra",
     whatsapp: "https://wa.me/5531971320866",
     vip: "https://docs.google.com/forms/d/e/1FAIpQLSeEvsAlYs96vbtXMNGPxGRTStibO9rn5TmLF9374HaKCt1c3g/viewform?usp=sf_link",
+    nat: "https://drive.google.com/drive/folders/1I_l-RcVrzCwWHtwFgWKE-ZcKhrJbb2AP",
   })
 );
 
