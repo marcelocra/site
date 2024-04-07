f44bc34648c77cfe229cc508cbc0d40827bbfa51
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2024-04-07 09:32:15 -0300

    remove unneded stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://marcelocra.dev",
-  integrations: [
-    preact(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [preact(), tailwind()],
 });
