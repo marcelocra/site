09f2eb2281f23a39dbe7851bb5993525d1519332
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2024-04-07 13:41:32 -0300

    update rss to use nuew format

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,20 @@
 import rss, { pagesGlobToRssItems } from "@astrojs/rss";
 import { DESCRIPTION, TITLE } from "../libs/constants";
+import { getCollection } from "astro:content";
 
 export async function GET(context) {
+  const posts = await getCollection("posts");
+
   return rss({
     title: TITLE,
     description: DESCRIPTION,
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.slug}/`,
+    })),
     customData: `<language>pt-br</language>`,
   });
 }
