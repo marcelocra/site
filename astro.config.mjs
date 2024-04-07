047e426b06d7ba309251a94ab16f5043e6fea861
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2024-04-07 05:41:36 -0300

    add tailwind support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from "astro/config";
-
 import preact from "@astrojs/preact";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://marcelocra.dev",
-  integrations: [preact()]
+  integrations: [
+    preact(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 });
