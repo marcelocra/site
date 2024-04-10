fb86da52d4caddb19bf7c58a97bf77015128ce7e
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2024-04-10 05:11:08 -0300

    mostly working

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,7 @@ import daisyui from "daisyui";
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "selector",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
