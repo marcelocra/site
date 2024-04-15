53f8946754d75b8cc06f7c4c8d1abdf5dfe123df
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2024-04-15 17:20:00 -0300

    remove dark mode support; start apply figma design

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,6 @@ import daisyui from "daisyui";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "selector",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
