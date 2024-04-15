a8709ac2bad1fa5b12f4c91b2227ff5db7a5f55e
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2024-04-15 01:15:25 -0300

    improve deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/env bash
 
-pnpm run build
+set -euo pipefail   
 
+if [[ "$(pwd)" != "$MCRA_PROJECTS_FOLDER/site" ]]; then
+  echo "You must run this script from the project root folder"
+  exit 1
+fi
+
+echo 'deleting dist folder...'
+rm -rf ./dist
+echo 'done!'
+
+echo 'building site...'
+pnpm run build
+echo 'done!'
+
+echo 'deploying site...'
 (cd dist \
   && deployctl deploy \
      --project=marcelocra \
      https://deno.land/std@0.220.0/http/file_server.ts)
+
+echo 'done!'
