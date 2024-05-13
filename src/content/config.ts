6e876ddc68208a94887cc11ddf2838125076eaae
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2024-05-13 15:21:31 -0300

    [idea] try to generate the timeline with content collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,22 +1,34 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-	type: 'content',
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
 
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
 
-		tags: z.array(z.string()).optional(),
+    tags: z.array(z.string()).optional(),
 
-		heroImage: z.string().optional(),
-		image: z.object({
-			url: z.string(),
-			alt: z.string(),
-		}).optional(),
-	}),
+    heroImage: z.string().optional(),
+    image: z
+      .object({
+        url: z.string(),
+        alt: z.string(),
+      })
+      .optional(),
+  }),
 });
 
-export const collections = { blog };
+const timeline = defineCollection({
+  type: "content",
+  schema: z.object({
+    name: z.string(),
+    year: z.number(),
+    logo: z.string(),
+    position: z.number(),
+  }),
+});
+
+export const collections = { blog, timeline };
