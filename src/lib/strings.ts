5eab384c94477b0f39161af1820623413652c9da
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2024-05-14 13:34:37 -0300

    remove unneeded files

---
+++ src/lib/strings.ts
@@ -0,0 +1,42 @@
+const defaultStrings = {
+  appName: "Utilitários",
+  appDescription: "Conjunto de utilitários para facilitar o dia a dia.",
+  appIndexInstructions:
+    "Escolha um dos apps disponíveis na barra lateral, à esquerda.",
+
+  numberToFullText: {
+    url: "/number-to-full-text",
+    title: "Número por extenso",
+    description: "Converte um número para sua forma por extenso.",
+    placeholder: "Digite um número e aperte Enter",
+    result: "Resultado",
+    convert: "Converter",
+  },
+  settings: {
+    url: "/settings",
+    title: "Configurações",
+  },
+};
+
+abstract class DeaultStrings {
+  strings: typeof defaultStrings;
+
+  constructor() {
+    this.strings = defaultStrings;
+  }
+}
+
+class PtBrStrings extends DeaultStrings {}
+
+class EnStrings extends DeaultStrings {
+  constructor() {
+    super();
+
+    this.strings.appDescription = "Set of utilities to make your day easier.";
+
+    this.strings.numberToFullText.title = "Number to full text";
+  }
+}
+
+const strings = new PtBrStrings();
+export const s = { ...strings.strings };
