d9209ecc76aa917eb053b635286c338022627c65
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2024-12-14 17:49:06 -0300

    feat: finish mobile hero

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Marcelo Almeida";
+export const SITE_TITLE = "marcelocra.dev";
 export const SITE_DESCRIPTION =
   "Seja bem vindo ao meu site pessoal! Aqui você encontra informações sobre mim e meus projetos.";
 
